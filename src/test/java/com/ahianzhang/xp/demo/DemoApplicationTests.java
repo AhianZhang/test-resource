6e2f8d23e16a2f14410b56ec66ce9386790d2ce7
ahianzhang <zhangxinshui@shopin.cn>
2020-07-15 12:29:41 +0800

    tests: improve code coverage

--- src/test/java/com/ahianzhang/xp/demo/DemoApplicationTests.java
+++ src/test/java/com/ahianzhang/xp/demo/DemoApplicationTests.java
@@ -1,7 +1,18 @@
+
 package com.ahianzhang.xp.demo;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class DemoApplicationTests {
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Test
+    void contextLoad() {
+        DemoApplication.main(new String[] {});
+    }
 }
